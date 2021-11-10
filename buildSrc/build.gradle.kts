7d0aa9c58b5f843eb5029e8ecf33d69be0abd205
Yakimov <10730@rambler.ru>
2021-11-10 18:21:19 +0700

    kotlindsl

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,11 @@
+repositories {
+    google()
+    mavenCentral()
+    jcenter() // Warning: this repository is going to shut down soon
+}
+
+plugins {
+    id 'com.android.application'
+    id 'kotlin-android'
+    id 'kotlin-kapt'
+}
